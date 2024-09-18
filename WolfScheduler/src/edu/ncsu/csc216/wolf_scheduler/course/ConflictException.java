e478ca5022969ac73c15cd92e8e18106ef0073c2
apiduru <apiduru@ncsu.edu>
2024-09-18 10:43:28 -0400

    Implemented ConflictExceptionTest

--- WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
@@ -7,5 +7,22 @@ package edu.ncsu.csc216.wolf_scheduler.course;
  * An exception is thrown when a conflict rises between activities.
  */
 public class ConflictException extends Exception {
+	/** ID used for serialization. */
+	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Constructor for ConflictException.
+	 * 
+	 * @param message the message specified for the Exception object.
+	 */
+	public ConflictException(String message) {
+		super(message);
+	}
+
+	/**
+	 * Constructor for ConflictException with an author specified default message.
+	 */
+	public ConflictException() {
+		this("Schedule conflict.");
+	}
 }
