6113248684a9e29e3e04b704e7d088903d9e29ce
apiduru <apiduru@ncsu.edu>
2024-09-18 10:12:22 -0400

    Added Conflict and ConflictException

---
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/Conflict.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.wolf_scheduler.course;
+
+/**
+ * Interface to check overlap/conflict between activities.
+ */
+public interface Conflict {
+
+	/**
+	 * Checks for a conflict with the new activity.
+	 * 
+	 * @param possibleConflictingActivity the activity being checked for conflicts.
+	 * @throws ConflictException if a conflict rises.
+	 */
+	void checkConflict(Activity possibleConflictingActivity) throws ConflictException;
+
+}
---
+++ WolfScheduler/src/edu/ncsu/csc216/wolf_scheduler/course/ConflictException.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package edu.ncsu.csc216.wolf_scheduler.course;
+
+/**
+ * An exception is thrown when a conflict rises between activities.
+ */
+public class ConflictException extends Exception {
+
+}
